a3fe1b3c97c6c9dd7241ecf1108061f6381e299e
Gizem Hançerlioğlu <39413875+gizemhancerlioglu@users.noreply.github.com>
2022-06-08 00:05:24 +0300

    passing props data to boxcomponent

--- src/BoxComponent.jsx
+++ src/BoxComponent.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 
 export default class BoxComponent extends Component {
+
     render() {
         return (
             <>
                 <div className="card border-info" style={{ maxWidth: "18rem", margin: "15px" }}>
-                    <div className="card-header">ID: 1</div>
+                    <div className="card-header">ID: {this.props.data.id}</div>
                     <div className="card-body text-info">
-                        <h5 className="card-title">Gizem</h5>
+                        <h5 className="card-title">{this.props.data.name}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
